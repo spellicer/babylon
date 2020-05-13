31e9ef43481ee955c62630109cb9b80428db53bf
Stephen Pellicer <spellicer@batson-cook.com>
2020-05-12 23:50:22 -0400

    fix camera position jitter from geolocation

--- camera.ts
+++ camera.ts
@@ -15,10 +15,14 @@ export class Camera extends DeviceOrientationCamera {
             map(() => this.position),
         );
         this.outMoved$ = fromEventPattern(cb => scene.onBeforeCameraRenderObservable.add(cb)).pipe(
-            tap(() => this.position.y = groundFinder(this.position) + 2),
+            map(() => this.position),
+            tap(position => position.y = groundFinder(position) + 2),
             map(() => this.getPositionString()),
         );
-        this.inPosition$.subscribe(position => this.position = position);
+        this.inPosition$.pipe(
+            tap(position => position.y = groundFinder(position) + 2),
+            tap(position => this.position = position),
+        ).subscribe(position => this.position = position);
     }
     getPositionString() {
         return `${this.position.x.toFixed(6)}, ${this.position.y.toFixed(6)}, ${this.position.z.toFixed(6)}`;
